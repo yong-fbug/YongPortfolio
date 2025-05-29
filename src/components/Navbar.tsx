ba46df1d86ac80e528840466dbdfe8bca325624c
Yong <yong@yong.flores>
2025-05-29 12:24:06 +0800

    update nav bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,20 @@
 // Navbar.tsx
 import { useNavigate, useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { Menu, X } from "lucide-react";
 
 const sectionIds = ["home", "about", "projects", "contact"];
 
 export const Navbar = () => {
-  const [active, setActive] = useState("home");
+  const [active, setActive] = useState<string>("home");
+  const [menu, setMenu] = useState<boolean>(false)
   const navigate = useNavigate();
   const location = useLocation();
 
+  const isRootPage = location.pathname === "/" || location.pathname === "/YongPortfolio";
+
   const scrollToSection = (id: string) => {
-    if (location.pathname !== "/") {
+    if (!isRootPage) {
       navigate("/", { state: { scrollTo: id } });
     } else {
       const section = document.getElementById(id);
@@ -52,12 +56,27 @@ export const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 z-50 bg-white dark:bg-zinc-900 px-6 py-4 shadow">
       <div className="flex justify-between items-center">
         <div className="text-xl font-bold">Yong's Portfolio</div>
-        <div className="space-x-6">
+        
+        <button
+          onClick={() => setMenu((prev) => !prev)}
+          className="md:hidden text-zinc-800 dark:text-zinc-100"
+        >
+          {menu ? <X size={24} /> : <Menu size={24} />  }
+
+        </button>
+        
+        <div 
+          className={`${
+            menu ? "block" : "hidden"
+            } md:flex md:space-x-6 space-y-2 md:space-y-0 absolute md:static
+             bg-white dark:bg-zinc-900 left-0 w-full md:w-auto top-16 md:top-auto px-6
+             md:px-0 py-4 md:py-0 transition`}
+        >
           {sectionIds.map((id) => (
             <button
               key={id}
               onClick={() => scrollToSection(id)}
-              className={`capitalize transition ${
+              className={`capitalize transition text-left w-full md:w-auto ${
                 active === id
                   ? "text-blue-600 font-bold"
                   : "hover:text-blue-400"
