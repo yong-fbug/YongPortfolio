e14434b9a6b54b8359ef5270e37c3d2974378776
Yong <yong@yong.flores>
2025-06-04 10:15:04 +0800

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base: "/YongPortfolio/",
+  base: ".",
 })
+
+
+// "/YongPortfolio/",
